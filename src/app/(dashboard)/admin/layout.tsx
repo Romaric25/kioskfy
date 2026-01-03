8ea8a9fe184f4194f7336240f85bfa6ff23c9def
Romaric25 <53834690+Romaric25@users.noreply.github.com>
2026-01-03 19:32:19 +0100

    fix(admin): hide sidebar and header on login page

--- src/app/(dashboard)/admin/layout.tsx
+++ src/app/(dashboard)/admin/layout.tsx
@@ -11,7 +11,7 @@ export default function AdminDashboardLayout({
 }: {
     children: React.ReactNode;
 }) {
-    const paths = ["/admin/newspapers/render"]
+    const paths = ["/admin/newspapers/render", "/admin/login"]
     const pathname = usePathname();
     const showSidebar = !paths.includes(pathname);
 
@@ -27,7 +27,9 @@ export default function AdminDashboardLayout({
                 <AdminAppSidebar />
             </Activity>
             <SidebarInset>
-                <AdminSiteHeader />
+                <Activity mode={showSidebar ? "visible" : "hidden"}>
+                    <AdminSiteHeader />
+                </Activity>
                 <div className="flex flex-1 flex-col gap-4 p-4 pt-0 mt-4">
                     <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min p-6">
                         {children}
